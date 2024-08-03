4b802c91f7f4f22f2866c866df6d409b264d9ebb
Bertrand <bertrandrigal.pro@gmail.com>
2024-08-03 14:41:57 +0200

    add context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GlobalProvider } from './context/globalContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>,
 )
