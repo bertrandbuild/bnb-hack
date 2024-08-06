d17fdf5b8f0f48006be59123eb35219de2d8a6e2
YohanGh <YohanGH@proton.me>
2024-08-06 11:38:32 +0200

    Create Footer and navbar add in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import { Outlet } from 'react-router-dom';
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
 
 // import components
-// import NavBar from './components/NavBar';
-// import Footer from './components/Footer';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
       <div className="min-h-screen flex flex-col">
-        {/* <NavBar /> */}
+        <NavBar />
         <main className="flex-grow flex justify-center items-start px-4 py-0">
           <Outlet />
         </main>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
