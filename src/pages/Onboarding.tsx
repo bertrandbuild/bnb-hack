85500df1656495bd4335472c4c3c47c8de3a9d2f
YohanGh <YohanGH@proton.me>
2024-08-06 11:31:37 +0200

    Create visual page onboarding and create components strategyCard for print all strategy

---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+// import components
+import StrategyCard from "../components/StrategyCard";
+
+const strategies = [
+    {
+        title: "Risk balancing",
+        description: "Conservative",
+        buttonLabel: "Choose",
+      },
+      {
+        title: "Ichimoku cloud",
+        description: "Swing trading",
+        buttonLabel: "Choose",
+      },
+      {
+        title: "MA Cross - Daily",
+        description: "Swing trading",
+        buttonLabel: "Choose",
+      },
+]
+
+const Onboarding: React.FC = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="container mx-auto px-4 flex-grow">
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl font-bold">Choose your strategy</h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {strategies.map((strategy, index) => (
+            <StrategyCard
+              key={index}
+              title={strategy.title}
+              description={strategy.description}
+              buttonLabel={strategy.buttonLabel}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Onboarding;
