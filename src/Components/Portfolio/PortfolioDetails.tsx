363669b431857fda622ab8f5b4d374da2b7c7fa9
YohanGh <YohanGH@proton.me>
2024-08-09 16:49:34 +0200

    Create ui components for stat in components PortfolioDetails

--- src/Components/Portfolio/PortfolioDetails.tsx
+++ src/Components/Portfolio/PortfolioDetails.tsx
@@ -3,15 +3,30 @@ import React from "react";
 // import globalContext
 import { useGlobalContext } from "../../context/globalContext";
 
+// import components
+import StatItem from "../ui/StatItem";
+
 const PortfolioDetails: React.FC = () => {
   const { portfolio } = useGlobalContext();
+  const { initialQuoteSize, currentQuoteSize, pnl } = portfolio;
 
   return (
-    <div className="flex justify-between mb-2">
-      <span className="text-secondary">Initial {portfolio.initialQuoteSize} USDC</span>
-      <span className="text-secondary">Current {portfolio.currentQuoteSize} USDC</span>
-      {portfolio.pnl > 0 && <span className="text-secondary">{portfolio.pnl.toFixed(2)} PNL</span>}
-      <span className="text-secondary">${portfolio.totalUsd} USD - {portfolio.totalBtc} BTC</span>
+    <div className="flex">
+      <div className="stats stats-horizontal shadow rounded-lg w-full">
+        <StatItem title="Initial Value" value={`${initialQuoteSize} USDC`} />
+        <StatItem
+          title="Current Value"
+          value={`${currentQuoteSize.toFixed(2)} USDC`}
+        />
+        {pnl > 0 && <StatItem title="% PNL" value={pnl.toFixed(1)} />}
+      </div>
+      {/* TODO : Implementation in ???
+      <div className="stat">
+        <div className="stat-title"></div>
+        <div className="stat-value text-xl text-secondary">
+          ${portfolio.totalUsd} USD - {portfolio.totalBtc.toFixed(5)} BTC
+        </div>
+      </div> */}
     </div>
   );
 };
