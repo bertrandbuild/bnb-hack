e494c76b4b67e1dac32745cedc67c22705ef989d
YohanGh <YohanGH@proton.me>
2024-08-12 16:32:04 +0200

    Add components for backtesting results

--- src/Components/backtestingControls/AnalysisLauncher.tsx
+++ src/Components/backtestingControls/AnalysisLauncher.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { backtestingChartes } from "../../utils/backtestingChartes";
 import useStrategy from "../StrategyControls/useStrategy";
-import toast from "react-hot-toast";
 
-// import the components
+// import components
 import Loading from "../ui/Loading";
+import Message from "../chat/Message";
 
 interface AnalysisLauncherProps {
   analysisCount: number; // Number of analyses to be performed
@@ -15,9 +15,7 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
 }) => {
   const [isAnalyzing, setIsAnalyzing] = useState(false);
   const [selectedData, setSelectedData] = useState<string[]>([]);
-  const { runStrategy, isLoading } = useStrategy();
-
-  console.log("analysisCount", analysisCount);
+  const { runStrategy, isLoading, requestHash, llmResult } = useStrategy();
 
   // Retrieve the latest items from backtestingCharts based on analysisCount
   useEffect(() => {
@@ -25,20 +23,14 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
     setSelectedData(dataToAnalyze);
   }, [analysisCount]);
 
-  console.log("selectedData", selectedData);
-
   const handleStartAnalysis = async () => {
     setIsAnalyzing(true);
 
     for (let i = 0; i < analysisCount; i++) {
       try {
-        console.log(`Starting analysis ${i + 1}`);
         await runStrategy([selectedData[i]]);
-
-        toast.success(`Analysis ${i + 1} completed.`);
       } catch (error) {
         console.error(`Error during analysis ${i + 1}:`, error);
-        toast.error(`Error during analysis ${i + 1}.`);
         break; // Stop further analysis if an error occurs
       }
     }
@@ -47,6 +39,7 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
   };
 
   return (
+
     <div>
       <h2 className="text-lg font-bold mb-4 text-neutral">Start analysis</h2>
       <p className="text-lg mb-4 text-neutral">
@@ -59,7 +52,13 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
       >
         {isAnalyzing || isLoading ? "Analysis in progress..." : "Start analysis"}
       </button>
-      {isLoading && <Loading />}
+          {isLoading && <Loading />}
+          {requestHash && (
+            <p className="text-sm mt-6 text-primary text-ellipsis overflow-hidden">
+              Request Hash: {requestHash}
+            </p>
+          )}
+          {llmResult && <Message message={llmResult} />}
     </div>
   );
 };
