866eb9e012aa66a534f35799449951fb6ea2b18f
YohanGh <YohanGH@proton.me>
2024-08-12 20:44:34 +0200

    Modification of number of backtesting runs

--- src/Components/backtestingControls/StartBacktestingAuto.tsx
+++ src/Components/backtestingControls/StartBacktestingAuto.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import AnalysisLauncher from "./AnalysisLauncher";
 
 const StartBacktestingAuto: React.FC = () => {
-  const [value, setValue] = useState(2);
+  const [value, setValue] = useState(333);
 
   // Define a conversion factor (for example, each unit of value = 2 minutes)
   const conversionFactor = 1;
