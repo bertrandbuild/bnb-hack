c2f0ef5585b5e1c08113168e2a83ee3b98b28112
YohanGh <YohanGH@proton.me>
2024-08-12 14:42:35 +0200

    remove logic for tracking evolution backtesting

--- src/Components/backtestingControls/AnalysisLauncher.tsx
+++ src/Components/backtestingControls/AnalysisLauncher.tsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { backtestingChartes } from "../../utils/backtestingChartes";
+import useStrategy from "../StrategyControls/useStrategy";
+import toast from "react-hot-toast";
+
+// import the components
+import Loading from "../ui/Loading";
 
 interface AnalysisLauncherProps {
   analysisCount: number; // Number of analyses to be performed
-}
-
-interface AnalysisResult {
-  analysis: number;
-  result: string;
 }
 
 const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
   analysisCount,
 }) => {
   const [isAnalyzing, setIsAnalyzing] = useState(false);
-  const [results, setResults] = useState<AnalysisResult[]>([]);
   const [selectedData, setSelectedData] = useState<string[]>([]);
+  const { runStrategy, isLoading } = useStrategy();
 
   console.log("analysisCount", analysisCount);
 
@@ -25,24 +25,24 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
     setSelectedData(dataToAnalyze);
   }, [analysisCount]);
 
-  
-
   console.log("selectedData", selectedData);
 
   const handleStartAnalysis = async () => {
     setIsAnalyzing(true);
-    const newResults: AnalysisResult[] = [];
 
     for (let i = 0; i < analysisCount; i++) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const result = {
-        analysis: i + 1,
-        result: `Analysis results  + ${i + 1}`,
-      };
-      newResults.push(result);
+      try {
+        console.log(`Starting analysis ${i + 1}`);
+        await runStrategy([selectedData[i]]);
+
+        toast.success(`Analysis ${i + 1} completed.`);
+      } catch (error) {
+        console.error(`Error during analysis ${i + 1}:`, error);
+        toast.error(`Error during analysis ${i + 1}.`);
+        break; // Stop further analysis if an error occurs
+      }
     }
 
-    setResults(newResults);
     setIsAnalyzing(false);
   };
 
@@ -57,22 +57,9 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
         disabled={isAnalyzing}
         className="bg-blue-500 text-white p-2 rounded"
       >
-        {isAnalyzing ? "Analysis in progress..." : "Start analysis"}
+        {isAnalyzing || isLoading ? "Analysis in progress..." : "Start analysis"}
       </button>
-      {results.length > 0 && (
-        <div className="results mt-4">
-          <h3 className="text-lg mb-4 text-neutral">
-          Analysis results:
-          </h3>
-          <ul>
-            {results.map((result, index) => (
-              <li className="text-lg mb-4 text-neutral" key={index}>
-                {result.result}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {isLoading && <Loading />}
     </div>
   );
 };
