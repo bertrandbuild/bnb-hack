d0ad4a7efe2ccaade7929300c404a96c87f4378f
YohanGh <YohanGH@proton.me>
2024-08-12 10:17:34 +0200

    Logic for analysis launcher created new table in backtesting.png for analysis

--- src/Components/backtestingControls/AnalysisLauncher.tsx
+++ src/Components/backtestingControls/AnalysisLauncher.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { backtestingChartes } from "../../utils/backtestingChartes";
 
 interface AnalysisLauncherProps {
-  analysisCount: number; // Nombre d'analyses à effectuer
+  analysisCount: number; // Number of analyses to be performed
 }
 
 interface AnalysisResult {
@@ -13,20 +14,30 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
   analysisCount,
 }) => {
   const [isAnalyzing, setIsAnalyzing] = useState(false);
-  const [completed, setCompleted] = useState(0);
   const [results, setResults] = useState<AnalysisResult[]>([]);
+  const [selectedData, setSelectedData] = useState<string[]>([]);
+
+  console.log("analysisCount", analysisCount);
+
+  // Retrieve the latest items from backtestingCharts based on analysisCount
+  useEffect(() => {
+    const dataToAnalyze = backtestingChartes.slice(-analysisCount).reverse();
+    setSelectedData(dataToAnalyze);
+  }, [analysisCount]);
+
+  
+
+  console.log("selectedData", selectedData);
 
   const handleStartAnalysis = async () => {
     setIsAnalyzing(true);
-    setCompleted(0);
     const newResults: AnalysisResult[] = [];
 
     for (let i = 0; i < analysisCount; i++) {
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      setCompleted(i + 1);
       const result = {
         analysis: i + 1,
-        result: `Résultat de l'analyse  + ${i + 1}`,
+        result: `Analysis results  + ${i + 1}`,
       };
       newResults.push(result);
     }
@@ -37,32 +48,25 @@ const AnalysisLauncher: React.FC<AnalysisLauncherProps> = ({
 
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4 text-neutral">Lancer l'analyse</h2>
-      <p className="text-2xl font-bold mb-4 text-neutral">
-        Nombre d'analyses à effectuer: <strong>{analysisCount}</strong>
+      <h2 className="text-lg font-bold mb-4 text-neutral">Start analysis</h2>
+      <p className="text-lg mb-4 text-neutral">
+      Number of analyses to be performed: <strong className="text-blue-500">{analysisCount}</strong>
       </p>
       <button
         onClick={handleStartAnalysis}
         disabled={isAnalyzing}
         className="bg-blue-500 text-white p-2 rounded"
       >
-        {isAnalyzing ? "Analyse en cours..." : "Lancer l'analyse"}
+        {isAnalyzing ? "Analysis in progress..." : "Start analysis"}
       </button>
-      <div className="progress mt-4">
-        {isAnalyzing && (
-          <p className="text-2xl font-bold mb-4 text-neutral">
-            Analyse {completed}/{analysisCount} en cours...
-          </p>
-        )}
-      </div>
       {results.length > 0 && (
         <div className="results mt-4">
-          <h3 className="text-2xl font-bold mb-4 text-neutral">
-            Résultats de l'analyse:
+          <h3 className="text-lg mb-4 text-neutral">
+          Analysis results:
           </h3>
           <ul>
             {results.map((result, index) => (
-              <li className="text-2xl font-bold mb-4 text-neutral" key={index}>
+              <li className="text-lg mb-4 text-neutral" key={index}>
                 {result.result}
               </li>
             ))}
