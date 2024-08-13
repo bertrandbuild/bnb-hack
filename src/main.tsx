b10a0f56a0c17f15ec0ada79c89277accc5f9063
Bertrand <bertrandrigal.pro@gmail.com>
2024-08-13 11:22:05 +0200

    Install wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,13 @@ import OnboardingStrategy from "./pages/OnboardingStrategy.tsx";
 import OnboardingPortfolio from "./pages/OnboardingPortfolio.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import NotFound from "./pages/NotFound.tsx";
+// wagmi
+import { WagmiProvider } from 'wagmi'
+import { config } from './config/wagmi'
+// Tanstack Query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 
 const router = createBrowserRouter([
   {
@@ -41,8 +48,12 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalProvider>
-      <RouterProvider router={router} />
-      <Toaster position="top-right" />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" />
+        </QueryClientProvider>
+      </WagmiProvider>
     </GlobalProvider>
   </React.StrictMode>,
 )
