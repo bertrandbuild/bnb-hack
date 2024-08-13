933f579f293c4d1235ca8c40d0cb0a3d52e27df0
Bertrand <bertrandrigal.pro@gmail.com>
2024-08-13 11:26:44 +0200

    Move router in config folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+// Global Context
 import { GlobalProvider } from './context/globalContext';
+// Toaster
 import { Toaster } from 'react-hot-toast';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// import pages
-import OnboardingStrategy from "./pages/OnboardingStrategy.tsx";
-import OnboardingPortfolio from "./pages/OnboardingPortfolio.tsx";
-import Dashboard from "./pages/Dashboard.tsx";
-import NotFound from "./pages/NotFound.tsx";
+// Router
+import Router from './config/router';
 // wagmi
 import { WagmiProvider } from 'wagmi'
 import { config } from './config/wagmi'
@@ -19,38 +15,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <OnboardingStrategy />,
-      },
-      {
-        path: "/onboarding-portfolio",
-        element: <OnboardingPortfolio />,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
-
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalProvider>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
+          <Router />
           <Toaster position="top-right" />
         </QueryClientProvider>
       </WagmiProvider>
