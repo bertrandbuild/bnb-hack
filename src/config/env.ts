9f3a198117bc9fde42b503322ccd33659a08056c
Bertrand <bertrandrigal.pro@gmail.com>
2024-08-13 11:09:52 +0200

    Add greenfield env var

---
+++ src/config/env.ts
@@ -0,0 +1,7 @@
+export const IS_DEV=import.meta.env.DEV;
+// BNB Greenfield
+export const GRPC_URL = import.meta.env.VITE_GRPC_URL;
+export const GREENFIELD_RPC_URL = import.meta.env.VITE_GREENFIELD_RPC_URL;
+export const GREEN_CHAIN_ID = parseInt(import.meta.env.VITE_GREEN_CHAIN_ID);
+export const BSC_RPC_URL = import.meta.env.VITE_BSC_RPC_URL;
+export const BSC_CHAIN_ID = parseInt(import.meta.env.VITE_BSC_CHAIN_ID);
