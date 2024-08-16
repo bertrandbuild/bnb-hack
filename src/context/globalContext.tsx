ae19061eb77598140dfe7b199ac3e686c08925fc
YohanGh <YohanGH@proton.me>
2024-08-16 09:52:03 +0200

    Add console.log

--- src/context/globalContext.tsx
+++ src/context/globalContext.tsx
@@ -46,31 +46,43 @@ const defaultContext: IGlobalContextType = {
 export const GlobalContext = createContext<IGlobalContextType>(defaultContext);
 
 // Initialize the context with the default values and load from localStorage
-export const GlobalProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const GlobalProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [globalState, setGlobalState] = useState(defaultContext);
 
   const _saveToLocalStorage = (key: string, value: unknown) => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
+      console.log(`Saving ${key} to localStorage:`, value);
       localStorage.setItem(key, JSON.stringify(value));
     }
   };
 
   // Global function to update the context and save to localStorage
   const updateContext = (key: string, value: unknown) => {
-    setGlobalState(prevState => {
+    console.log(`Updating context key "${key}" with value:`, value);
+    setGlobalState((prevState) => {
       const newState = { ...prevState, [key]: value };
+
+      console.log(`Previous state:`, prevState);
+      console.log(`New state:`, newState);
+
       _saveToLocalStorage(key, value);
+      console.log("New global state after update:", newState);
       return newState;
     });
   };
 
   // Initialize the context with the default values and load from localStorage
   useEffect(() => {
+    console.log("Loading initial state from localStorage...");
+    const loadedState = loadAllFromLocalStorage(initialLocalStorageConfig);
     // Init state by loading from localStorage
-    setGlobalState(prevState => ({
+    setGlobalState((prevState) => ({
       ...prevState,
-      ...loadAllFromLocalStorage(initialLocalStorageConfig)
+      ...loadedState,
     }));
+    console.log("Global state after loading from localStorage:", globalState);
   }, []);
 
   return (
